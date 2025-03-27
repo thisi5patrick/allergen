bcbf313e21d1cb4fb55e06de446ce1aba67a6685
thisi5patrick <patrykbielen97@gmail.com>
2025-03-27 21:31:24 +0100

    Added symptom entries and types to admin panel

--- allergy/admin.py
+++ allergy/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from allergy.models import SymptomEntry, SymptomType
+
+
+@admin.register(SymptomType)
+class SymptomTypeAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("name", "user")
+
+
+@admin.register(SymptomEntry)
+class SymptomEntryAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("user", "entry_date", "symptom_type", "intensity")
